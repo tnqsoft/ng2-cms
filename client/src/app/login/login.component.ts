0c1f3ab80cb528abcb4169c185dd1f0deb6300ab
Nguyen Nhu Tuan <tuanquynh0508@gmail.com>
2017-01-23 18:04:50 +0700

    Add Login Validate

--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -8,13 +8,17 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent implements OnInit {
 
+  private model: any = {};
+  private loading: boolean = false;
+  private error = '';
+
   constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
   login() {
-    this.router.navigate(['/dashboard']);
+    // this.router.navigate(['/dashboard']);
   }
 
 }
